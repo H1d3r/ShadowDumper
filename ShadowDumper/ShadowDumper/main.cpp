35e738daa14451d10c346b7cfb00800ea1934b83
Usman Sikander <76246439+Offensive-Panda@users.noreply.github.com>
2024-11-10 20:45:13 +0300

    Update main.cpp

--- ShadowDumper/ShadowDumper/main.cpp
+++ ShadowDumper/ShadowDumper/main.cpp
@@ -1,3 +1,23 @@
+/*
+ * © 2024 Usman Sikander - Offensive Panda
+ *
+ * This software is provided under the MIT License.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #define _CRT_SECURE_NO_WARNINGS // Suppress security warnings
 
 #include <iostream>
@@ -42,7 +62,7 @@ void DisplayMenu() {
     SetConsoleColor(GREEN);
     std::cout << "\n*******************************" << std::endl;
     SetConsoleColor(CYAN);
-    std::cout << "ShadowDumper: A powerful tool created in C/C++ to dump LSASS memory using multiple advanced techniques � Offensive-Panda" << std::endl;
+    std::cout << "ShadowDumper: A powerful tool created in C/C++ to dump LSASS memory using multiple advanced techniques © Offensive-Panda" << std::endl;
     SetConsoleColor(YELLOW);
     std::cout << "Note: All Dump files will be stored in C:\\Users\\Public" << std::endl;
     SetConsoleColor(YELLOW);
